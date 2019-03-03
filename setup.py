df5205afb11cf4541ae4fb4c9d0b0dff39152fd6
Manuel <m_koes17@uni-muenster.de>
2019-03-03 17:11:51 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+import setuptools
+from setuptools.command.install_lib import install_lib
+import distutils
+import os
+import sys
+
+
+PACKAGE_NAME = "src"
+AUTHORS = ""
+MAIl = ""
+# We store our version number in a simple text file:
+version_path = os.path.join(os.path.dirname(__file__), PACKAGE_NAME, "version.txt")
+with open(version_path, "r") as version_file:
+    version = version_file.read().strip()
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=version,
+    packages=[PACKAGE_NAME],
+    package_dir={PACKAGE_NAME: ""},
+    description="",
+    package_data={},
+    exclude_package_data={"": ["Documentation"]},
+    requires=[],
+    long_description="",
+    author=AUTHORS,
+    author_email=MAIl,
+    url="",
+    license="",
+    platforms="",
+    classifiers=["Development Status :: 4 - Beta", "Environment :: Console"],
+)
